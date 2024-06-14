96fe459c1234e4e8c653e4bad537a90aae214e81
Mari1921 <167789893+Mari1921@users.noreply.github.com>
2024-06-14 18:31:25 -0300

    Add files via upload

---
+++ Desafio.js
@@ -0,0 +1,18 @@
+let nomeHeroi = "Shiro Uchiha"
+let heroiXp = 3000;
+
+let nivel = heroiXp <= 1000 ? "Ferro": "Nenhum";
+heroiXp > 1001 && heroiXp <= 2000 ? "Bronze" :
+heroiXp > 2001 && heroiXp <= 5000 ? "Prata" :
+heroiXp > 5001 && heroiXp <= 7000 ? "Ouro" :
+heroiXp > 7001 && heroiXp <= 8000 ? "Platina" :
+heroiXp > 8001 && heroiXp <= 9000 ? "Ascendente" :
+heroiXp > 9001 && heroiXp <= 10000 ? "Imortal" :
+heroiXp > 10000 ? "Radiante" : "Nenhum";
+console.log(nivel);
+console.log("O heroi" + " " + nomeHeroi + " " + "está no nivel de" + " " + nivel)
+
+
+
+
+
